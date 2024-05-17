13fba5195ac5d50813415c6b170d1e18b2b2eae4
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-17 20:06:07 +0330

    react-tabs

--- components/products/ProductsTab.jsx
+++ components/products/ProductsTab.jsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Link from "next/link";
 import Product from "./Product";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 const ProductsTab = ({ tabList, tabPanel }) => {
   return (
@@ -9,23 +12,29 @@ const ProductsTab = ({ tabList, tabPanel }) => {
           <h2>منو محصولات</h2>
         </div>
 
-        <ul className="filters_menu">
-          {tabList.map((list, index) => (
-            <li key={index}>{list}</li>
-          ))}
-        </ul>
-
-        <div className="filters-content">
-          {tabPanel.map((panel, index) => (
-            <div key={index} className="row grid">
-              {panel.map((product) => (
-                <div key={product.id} className="col-sm-6 col-lg-4">
-                  <Product product={product} />
-                </div>
+        <Tabs selectedTabClassName="active">
+          <TabList>
+            <ul className="filters_menu">
+              {tabList.map((list, index) => (
+                <Tab key={index}>{list}</Tab>
               ))}
-            </div>
-          ))}
-        </div>
+            </ul>
+          </TabList>
+
+          <div className="filters-content">
+            {tabPanel.map((panel, index) => (
+              <TabPanel key={index}>
+                <div className="row grid">
+                  {panel.map((product) => (
+                    <div key={product.id} className="col-sm-6 col-lg-4">
+                      <Product product={product} />
+                    </div>
+                  ))}
+                </div>
+              </TabPanel>
+            ))}
+          </div>
+        </Tabs>
 
         <div className="btn-box">
           <Link href="/menu">مشاهده بیشتر</Link>
