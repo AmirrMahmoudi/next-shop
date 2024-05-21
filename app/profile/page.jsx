446a18232efa1e57efe7811c43258f93c05ff812
AmirrMahmoudi <amirhosssein.js@gmail.com>
2024-05-21 18:54:29 +0330

    Edit user information

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,3 +1,4 @@
+import EditForm from "@/components/profile/info/EditForm";
 import { getFetch } from "@/utils/fetch";
 import { cookies } from "next/headers";
 import React from "react";
@@ -8,39 +9,11 @@ const ProfilePage = async () => {
     Authorization: `Bearer ${token.value}`,
   });
 
-  console.log(user);
+  //   console.log(user);
   return (
     <div className="vh-70">
-      <div className="row g-4">
-        <div className="col col-md-6">
-          <label className="form-label">نام و نام خانوادگی</label>
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={user.name}
-          />
-        </div>
-        <div className="col col-md-6">
-          <label className="form-label">ایمیل</label>
-          <input
-            type="text"
-            className="form-control"
-            defaultValue={user.email}
-          />
-        </div>
-        <div className="col col-md-6">
-          <label className="form-label">شماره تلفن</label>
-          <input
-            type="text"
-            disabled
-            className="form-control"
-            defaultValue={user.cellphone}
-          />
-        </div>
-      </div>
-      <button type="submit" className="btn btn-primary mt-4">
-        ویرایش
-      </button>
+      <EditForm user={user} />
+ 
     </div>
   );
 };
